fa9fa4914538ca4b3b6d932772acd692b29b77a7
scp <scp@archlinuxscp.xyz>
2023-05-06 03:21:22 +0800

    add diffview and neogit plugins

--- lua/user/mappings.lua
+++ lua/user/mappings.lua
@@ -1,7 +1,10 @@
 return {
   n = {
         ["<C-q>"] = false,
-        ["<leader><space>"] = { function() require("telescope.builtin").oldfiles() end, desc = "Find history" }
+        ["<leader><space>"] = { function() require("telescope.builtin").oldfiles() end, desc = "Find history" },
+        ["<leader>gg"] = { "<cmd>Neogit<cr>" },
+        ["<leader>gH"] = { "<cmd>DiffviewFileHistory<cr>" },
+        ["<leader>tl"] = false, --disable lazy git key
   },
   t = {
         ["<C-\\>"] = "<C-\\><C-n>",
--- lua/user/plugins/user.lua
+++ lua/user/plugins/user.lua
@@ -14,4 +14,16 @@ return {
       }
     end
   },
+  {
+    "sindrets/diffview.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    lazy = false,
+    opts = { diff_binaries = false },
+  },
+  {
+    "TimUntersberger/neogit",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    lazy = false,
+    opts = { integrations = { diffview = true } }
+  }
 }
